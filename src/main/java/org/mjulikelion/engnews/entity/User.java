03218244f3d502ecff4d27551969b81f642b1417
juryheed <rabitangl20@naver.com>
2024-11-13 02:49:21 +0900

    feat: User Entity 수정 - #5

--- src/main/java/org/mjulikelion/engnews/entity/User.java
+++ src/main/java/org/mjulikelion/engnews/entity/User.java
@@ -1,12 +1,12 @@
 package org.mjulikelion.engnews.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -17,8 +17,23 @@ import lombok.NoArgsConstructor;
 public class User extends BaseEntity {
     @Column(nullable = false)
     private String name;
+
     @Column(nullable = false)
     private String email;
+
     @Column(nullable = false)
     private String password;
+
+    //카테고리
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Category> categories;
+
+    //찜한 뉴스
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<ArticleLike> articleLikes;
+
+    //찜한 단어
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,orphanRemoval = true)
+    private  List<WordLike> wordLikes;
+
 }
