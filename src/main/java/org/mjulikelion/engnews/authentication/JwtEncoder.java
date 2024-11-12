d564235f9f9e0af1643e9f44fb1c2550d97055e1
aaahyunseo <hs030608@naver.com>
2024-11-13 00:48:28 +0900

    feat: JwtEncoder 추가 - #3

---
+++ src/main/java/org/mjulikelion/engnews/authentication/JwtEncoder.java
@@ -0,0 +1,28 @@
+package org.mjulikelion.engnews.authentication;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@AllArgsConstructor
+public class JwtEncoder {
+
+    public static final String TOKEN_TYPE = "Bearer ";
+
+    public static String encode(String token) {
+        String cookieValue = TOKEN_TYPE + token;
+        return URLEncoder.encode(cookieValue, StandardCharsets.UTF_8).replace("+", "%20");
+    }
+
+    public static String decode(String cookieValue) {
+        String value = URLDecoder.decode(cookieValue, StandardCharsets.UTF_8);
+        if (value.startsWith(TOKEN_TYPE)) {
+            return value.substring(TOKEN_TYPE.length());
+        }
+        throw new UnauthorizedException(ErrorCode.INVALID_TOKEN, "token decode 실패");
+    }
+}
