fda1e404d5d91f28ac24984235914afa88e7ea56
juryheed <rabitangl20@naver.com>
2024-11-19 04:08:12 +0900

    feature:  KeywordDto 생성 - #15

---
+++ src/main/java/org/mjulikelion/engnews/dto/request/keyword/KeywordDto.java
@@ -0,0 +1,21 @@
+package org.mjulikelion.engnews.dto.request.keyword;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KeywordDto {
+
+    @NotNull(message = "관련 카테고리를 입력해 주세요")
+    private UUID categoryId;
+
+    @NotEmpty(message = "키워드를 입력해주세요.")
+    private String keyword;
+}
