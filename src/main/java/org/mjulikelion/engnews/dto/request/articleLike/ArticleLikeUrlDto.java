b2ddc7bf8b7e5ba6104826f4237d4e5abf60e52d
aaahyunseo <hs030608@naver.com>
2024-11-20 02:12:05 +0900

    feat: ArticleLikeUrlDto 생성 - #20

---
+++ src/main/java/org/mjulikelion/engnews/dto/request/articleLike/ArticleLikeUrlDto.java
@@ -0,0 +1,10 @@
+package org.mjulikelion.engnews.dto.request.articleLike;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+
+@Getter
+public class ArticleLikeUrlDto {
+    @NotEmpty(message = "저장할 기사의 url을 입력해주세요.")
+    private String originalUrl;
+}
