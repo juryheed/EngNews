40d8abcc1d48d901f0b1e84a54aa415077613d20
juryheed <rabitangl20@naver.com>
2024-11-20 02:35:18 +0900

    feature: KeywordRepository에 메서드 추가 - #19

--- src/main/java/org/mjulikelion/engnews/repository/KeywordRepository.java
+++ src/main/java/org/mjulikelion/engnews/repository/KeywordRepository.java
@@ -5,8 +5,10 @@ import org.mjulikelion.engnews.entity.Keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface KeywordRepository extends JpaRepository<Keyword, UUID> {
     List<Keyword> findAllByCategory(Category category);
+    Optional<Keyword> findById(UUID id);
 }
