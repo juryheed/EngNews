fdd9c8fad29c2a493f15d1376f3febb4397f7ddd
aaahyunseo <hs030608@naver.com>
2024-11-21 00:31:52 +0900

    feat: NaverNewsController 키워드로 찾기 기능 수정 - #25

--- src/main/java/org/mjulikelion/engnews/controller/NaverNewsController.java
+++ src/main/java/org/mjulikelion/engnews/controller/NaverNewsController.java
@@ -2,11 +2,9 @@ package org.mjulikelion.engnews.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.mjulikelion.engnews.authentication.AuthenticatedUser;
 import org.mjulikelion.engnews.dto.request.article.CategoryRequestDto;
 import org.mjulikelion.engnews.dto.response.ResponseDto;
 import org.mjulikelion.engnews.dto.response.article.CategoryArticleDto;
-import org.mjulikelion.engnews.entity.User;
 import org.mjulikelion.engnews.service.NaverNewsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,15 +19,15 @@ import java.util.UUID;
 public class NaverNewsController {
     private final NaverNewsService naverNewsService;
 
-    @GetMapping("/{categoryId}")
-    public String getNaverNews(@AuthenticatedUser User user, @PathVariable UUID categoryId) {
-        return naverNewsService.getNaverNews(user,categoryId);
+    @GetMapping("/{keywordId}")
+    public String getNaverNews(@PathVariable UUID keywordId) {
+        return naverNewsService.getNaverNews(keywordId);
     }
 
     @GetMapping("/category")
     public ResponseEntity<ResponseDto<List<CategoryArticleDto>>> getNewsByCategory(@RequestBody @Valid CategoryRequestDto categoryRequestDto) {
         List<CategoryArticleDto> articles = naverNewsService.getArticlesByCategory(categoryRequestDto.getCategory());
-        return ResponseEntity.ok(ResponseDto.res(HttpStatus.OK, categoryRequestDto.getCategory()+" 카테고리 기사 조회 성공", articles));
+        return ResponseEntity.ok(ResponseDto.res(HttpStatus.OK, categoryRequestDto.getCategory()+" 카테고리 기사 목록 조회 성공", articles));
     }
 
 }
