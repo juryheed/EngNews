bad39ca47831a8a7b6404962da5bb095e69c5fb3
juryheed <rabitangl20@naver.com>
2024-11-22 02:34:21 +0900

    feature: 번역 테스트 컨트롤러 - #27

--- src/main/java/org/mjulikelion/engnews/test.java
+++ src/main/java/org/mjulikelion/engnews/test.java
@@ -1,15 +1,23 @@
 package org.mjulikelion.engnews;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.mjulikelion.engnews.service.TranslationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 public class test {
 
+    private final TranslationService  translationService;
+
     @GetMapping("/test")
     public String test(){
         return "test";
+    }
+
+    @GetMapping("/transelate")
+    public String translate() {
+        return translationService.translate();
     }
 }
