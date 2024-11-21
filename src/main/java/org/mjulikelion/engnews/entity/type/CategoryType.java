55fa6d4b5bd6cfb424848db221c47a24a973eecf
aaahyunseo <hs030608@naver.com>
2024-11-22 00:25:29 +0900

    feat: CategoryType 추가 - #29

--- src/main/java/org/mjulikelion/engnews/entity/type/CategoryType.java
+++ src/main/java/org/mjulikelion/engnews/entity/type/CategoryType.java
@@ -1,5 +1,8 @@
 package org.mjulikelion.engnews.entity.type;
 
 public enum CategoryType {
-    ECONOMY, ENTERTAINTMENT,POLITICS,SPORTS
+    // 네이버 뉴스 카테고리
+    정치, 경제, 사회, 생활문화, IT과학, 세계,
+    // NYT 뉴스 카테고리
+    US, WORLD, BUSINESS, ARTS, LIFESTYLE, OPINION;
 }
