ed5a86a6608bb60d4f05912bfa80d13af02ec909
juryheed <rabitangl20@naver.com>
2024-11-22 02:34:47 +0900

    feature: 엔티티 컬럼 추가 - #30

--- src/main/java/org/mjulikelion/engnews/entity/Keyword.java
+++ src/main/java/org/mjulikelion/engnews/entity/Keyword.java
@@ -17,6 +17,9 @@ public class Keyword extends BaseEntity {
     @Column(nullable = false)
     private String keyword;
 
+    @Column(nullable = false)
+    private String translated;
+
     //카테고리와 관계
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "categories_id")
