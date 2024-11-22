45bcbc057f0c62261acd66314d9cdc99ab4d91e8
aaahyunseo <hs030608@naver.com>
2024-11-23 01:25:23 +0900

    feat: NYTService 생성 - #39

---
+++ src/main/java/org/mjulikelion/engnews/service/NYTService.java
@@ -0,0 +1,129 @@
+package org.mjulikelion.engnews.service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.mjulikelion.engnews.dto.request.article.ArticleRequestDto;
+import org.mjulikelion.engnews.dto.response.article.ArticleDto;
+import org.mjulikelion.engnews.dto.response.article.CategoryArticleDto;
+import org.mjulikelion.engnews.entity.Category;
+import org.mjulikelion.engnews.entity.Keyword;
+import org.mjulikelion.engnews.entity.User;
+import org.mjulikelion.engnews.exception.ErrorCode;
+import org.mjulikelion.engnews.exception.UnauthorizedException;
+import org.mjulikelion.engnews.repository.CategoryRepository;
+import org.mjulikelion.engnews.repository.KeywordRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class NYTService {
+
+    @Value("${nyt.client.key}")
+    private String clientId;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final KeywordRepository keywordRepository;
+    private final CategoryRepository categoryRepository;
+    private final ArticleLikeService articleLikeService;
+
+    // 키워드로 NYT 뉴스 조회
+    public List<CategoryArticleDto> getNYTNewsByKeyword(User user) {
+        List<Category> categories = categoryRepository.findAllByUser(user);
+        List<Keyword> keywords = keywordRepository.findAllByCategoryIn(categories);
+
+        List<CategoryArticleDto> allArticles = new ArrayList<>();
+
+        for (Keyword keyword : keywords) {
+            String keywordName = keyword.getKeyword();
+
+            String url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+                    + "?q=" + keywordName
+                    + "&api-key=" + clientId;
+
+            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode rootNode = objectMapper.readTree(response.getBody());
+                JsonNode articles = rootNode.path("response").path("docs");
+
+                for (JsonNode article : articles) {
+                    String title = article.path("headline").path("main").asText();
+                    String link = article.path("web_url").asText();
+                    String imageUrl = crawlImageUrlFromArticle(link);
+
+                    allArticles.add(CategoryArticleDto.from(title, link, imageUrl));
+                }
+            } catch (Exception e) {
+                throw new UnauthorizedException(ErrorCode.INVALID_ARTICLE);
+            }
+        }
+        return allArticles;
+    }
+
+    // 카테고리로 NYT 뉴스 조회
+    public List<CategoryArticleDto> getNYTByCategory(String category) {
+        String filter = "news_desk:(" + category + ")";
+        String url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+                + "?fq=" + filter
+                + "&api-key=" + clientId;
+
+        List<CategoryArticleDto> articles = new ArrayList<>();
+
+        try {
+            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(response.getBody());
+            JsonNode articlesNode = rootNode.path("response").path("docs");
+
+            for (JsonNode article : articlesNode) {
+                String title = article.path("headline").path("main").asText();
+                String link = article.path("web_url").asText();
+                String imageUrl = crawlImageUrlFromArticle(link);
+                articles.add(CategoryArticleDto.from(title, link, imageUrl));
+            }
+        } catch (IOException e) {
+            throw new UnauthorizedException(ErrorCode.INVALID_ARTICLE);
+        }
+        return articles;
+    }
+
+    // 단건 기사 조회
+    public ArticleDto getNYTNews(ArticleRequestDto articleRequestDto) {
+        String url = articleRequestDto.getUrl();
+        String[] article = articleLikeService.getTitleImageAndContentFromUrl(url);
+
+        return ArticleDto.from(
+                article[0],
+                article[1],
+                article[2]
+        );
+    }
+
+    private String crawlImageUrlFromArticle(String articleLink) {
+        try {
+            Document doc = Jsoup.connect(articleLink).get();
+            Elements metaTags = doc.select("meta[property=og:image]");
+            if (!metaTags.isEmpty()) {
+                return metaTags.first().attr("content");
+            } else {
+                return "https://via.placeholder.com/150";
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "https://via.placeholder.com/150";
+        }
+    }
+}
