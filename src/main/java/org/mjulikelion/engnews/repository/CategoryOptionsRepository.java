234e8a74de38d0ad0c8605472c307fc5cc43987f
aaahyunseo <hs030608@naver.com>
2024-11-24 04:27:57 +0900

    feat: CategoryOptionsRepository 추가 - #43

---
+++ src/main/java/org/mjulikelion/engnews/repository/CategoryOptionsRepository.java
@@ -0,0 +1,12 @@
+package org.mjulikelion.engnews.repository;
+
+import org.mjulikelion.engnews.entity.CategoryOptions;
+import org.mjulikelion.engnews.entity.type.CategoryType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CategoryOptionsRepository extends JpaRepository<CategoryOptions, UUID> {
+    Optional<CategoryOptions> findByCategoryType(CategoryType categoryType);
+}
