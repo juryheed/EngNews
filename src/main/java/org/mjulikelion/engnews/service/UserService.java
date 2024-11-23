f18c8a0e44bcdb3a34fbfddf201f1fc6c9bafc96
juryheed <rabitangl20@naver.com>
2024-11-24 05:48:17 +0900

    feat: UserService의 getMypage메서드 수정 - #41

--- src/main/java/org/mjulikelion/engnews/service/UserService.java
+++ src/main/java/org/mjulikelion/engnews/service/UserService.java
@@ -3,13 +3,19 @@ package org.mjulikelion.engnews.service;
 import lombok.RequiredArgsConstructor;
 import org.mjulikelion.engnews.authentication.PasswordHashEncryption;
 import org.mjulikelion.engnews.dto.response.user.MypageDto;
+import org.mjulikelion.engnews.entity.Category;
+import org.mjulikelion.engnews.entity.Keyword;
 import org.mjulikelion.engnews.entity.User;
 import org.mjulikelion.engnews.exception.ConflictException;
 import org.mjulikelion.engnews.exception.ErrorCode;
 import org.mjulikelion.engnews.exception.NotFoundException;
 import org.mjulikelion.engnews.exception.UnauthorizedException;
+import org.mjulikelion.engnews.repository.CategoryRepository;
+import org.mjulikelion.engnews.repository.KeywordRepository;
 import org.mjulikelion.engnews.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,13 +23,18 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordHashEncryption passwordHashEncryption;
+    private final CategoryRepository categoryRepository;
+    private final KeywordRepository keywordRepository;
 
     public MypageDto getMypage(User user) {
-        return MypageDto.builder()
-                .id(user.getId())
-                .name(user.getName())
-                .email(user.getEmail())
-                .build();
+
+        // 유저와 연결된 카테고리 리스트 가져오기
+        List<Category> categories = categoryRepository.findAllByUser(user);
+
+        // 카테고리 리스트와 연결된 키워드 가져오기
+        List<Keyword> keywords = keywordRepository.findAllByCategoryIn(categories);
+
+        return MypageDto.from(user, keywords, categories);
     }
 
     public void validateIsPasswordMatches(String requestedPassword, String userPassword) {
