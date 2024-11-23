185dd77f4c850ddbecd7a6521a4d2155b30889fd
aaahyunseo <hs030608@naver.com>
2024-11-24 04:27:40 +0900

    feat: CategoryOptions 엔티티 추가 - #43

---
+++ src/main/java/org/mjulikelion/engnews/entity/CategoryOptions.java
@@ -0,0 +1,20 @@
+package org.mjulikelion.engnews.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.mjulikelion.engnews.entity.type.CategoryType;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "category_options")
+public class CategoryOptions extends BaseEntity {
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CategoryType categoryType;
+}
