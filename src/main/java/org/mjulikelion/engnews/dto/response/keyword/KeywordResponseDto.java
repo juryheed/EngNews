208d282a966f4ea6124804738154498673722a8e
juryheed <rabitangl20@naver.com>
2024-11-24 23:23:24 +0900

    feat: KeywordResponseDto원래대로 돌리기 - #58

--- src/main/java/org/mjulikelion/engnews/dto/response/keyword/KeywordResponseDto.java
+++ src/main/java/org/mjulikelion/engnews/dto/response/keyword/KeywordResponseDto.java
@@ -10,18 +10,13 @@ import java.util.UUID;
 @Getter
 @Builder
 public class KeywordResponseDto {
-    private CategoryType categoryOption;
-    private UUID userCategoryId;
+    private UUID id;
     private String keyword;
-    private UUID keywordId;
-
 
     public static  KeywordResponseDto from(Keyword keyword){
         return KeywordResponseDto.builder()
-                .categoryOption(keyword.getCategory().getCategoryOptions().getCategoryType())    //카테고리옵션 아이디
-                .userCategoryId(keyword.getCategory().getId())  //유저가 설정한 키워드
-                .keyword(keyword.getKeyword())  //키워드 이름
-                .keywordId(keyword.getId())    //키워드 아이디
+                .id(keyword.getId())
+                .keyword(keyword.getKeyword())
                 .build();
     }
 }
