1f40780b5875b9187ec2fda22aff648fd59590bc
juryheed <rabitangl20@naver.com>
2024-11-24 22:52:22 +0900

    feat: KeywordResponseDto 수정 - #58

--- src/main/java/org/mjulikelion/engnews/dto/response/keyword/KeywordResponseDto.java
+++ src/main/java/org/mjulikelion/engnews/dto/response/keyword/KeywordResponseDto.java
@@ -3,19 +3,25 @@ package org.mjulikelion.engnews.dto.response.keyword;
 import lombok.Builder;
 import lombok.Getter;
 import org.mjulikelion.engnews.entity.Keyword;
+import org.mjulikelion.engnews.entity.type.CategoryType;
 
 import java.util.UUID;
 
 @Getter
 @Builder
 public class KeywordResponseDto {
-    private UUID id;
+    private CategoryType categoryOption;
+    private UUID userCategoryId;
     private String keyword;
+    private UUID keywordId;
+
 
     public static  KeywordResponseDto from(Keyword keyword){
         return KeywordResponseDto.builder()
-                .id(keyword.getId())
-                .keyword(keyword.getKeyword())
+                .categoryOption(keyword.getCategory().getCategoryOptions().getCategoryType())    //카테고리옵션 아이디
+                .userCategoryId(keyword.getCategory().getId())  //유저가 설정한 키워드
+                .keyword(keyword.getKeyword())  //키워드 이름
+                .keywordId(keyword.getId())    //키워드 아이디
                 .build();
     }
 }
