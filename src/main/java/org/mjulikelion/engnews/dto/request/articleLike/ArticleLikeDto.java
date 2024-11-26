8e1e0895b89a8172b3e6c1a0459f1bae88dcd23e
aaahyunseo <hs030608@naver.com>
2024-11-26 22:13:15 +0900

    refactor: ArticleLikeDto 이름 변경- #72

--- src/main/java/org/mjulikelion/engnews/dto/request/articleLike/ArticleLikeDto.java
+++ src/main/java/org/mjulikelion/engnews/dto/request/articleLike/ArticleLikeDto.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 @Getter
-public class ArticleLikeUrlDto {
+public class ArticleLikeDto {
     @NotEmpty(message = "저장할 기사의 url을 입력해주세요.")
     private String originalUrl;
     @NotEmpty(message = "저장할 기사의 사이트를 입력해주세요.")
