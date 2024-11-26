52f11ba4f7c004c6d8eb8bf3e481f6ec0fa2a657
aaahyunseo <hs030608@naver.com>
2024-11-26 22:09:25 +0900

    feat: ArticleLikeController 뉴스 별 찜한 뉴스 목록 조회 구분 - #72

--- src/main/java/org/mjulikelion/engnews/controller/ArticleLikeController.java
+++ src/main/java/org/mjulikelion/engnews/controller/ArticleLikeController.java
@@ -21,16 +21,22 @@ public class ArticleLikeController {
 
     private final ArticleLikeService articleLikeService;
 
-    @GetMapping
-    public ResponseEntity<ResponseDto<ArticleLikeListResponseDto>> getAllArticleLike(@AuthenticatedUser User user) {
-        ArticleLikeListResponseDto articleLikeListResponseDto = articleLikeService.getAllArticleLike(user);
-        return new ResponseEntity<>(ResponseDto.res(HttpStatus.OK, "찜한 기사 리스트 조회 완료", articleLikeListResponseDto), HttpStatus.OK);
+    @GetMapping("/naver")
+    public ResponseEntity<ResponseDto<ArticleLikeListResponseDto>> getNaverArticleLikes(@AuthenticatedUser User user) {
+        ArticleLikeListResponseDto articleLikeListResponseDto = articleLikeService.getNaverArticleLikes(user);
+        return new ResponseEntity<>(ResponseDto.res(HttpStatus.OK, "네이버 뉴스 찜한 기사 리스트 조회 완료", articleLikeListResponseDto), HttpStatus.OK);
+    }
+
+    @GetMapping("/nyt")
+    public ResponseEntity<ResponseDto<ArticleLikeListResponseDto>> getNytArticleLikes(@AuthenticatedUser User user) {
+        ArticleLikeListResponseDto articleLikeListResponseDto = articleLikeService.getNytArticleLikes(user);
+        return new ResponseEntity<>(ResponseDto.res(HttpStatus.OK, "NYT 뉴스 찜한 기사 리스트 조회 완료", articleLikeListResponseDto), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<ResponseDto<Void>> saveArticleLike(@AuthenticatedUser User user,
                                                              @RequestBody @Valid ArticleLikeUrlDto articleLikeUrlDto) {
-        articleLikeService.saveArticleLike(user, articleLikeUrlDto.getOriginalUrl());
+        articleLikeService.saveArticleLike(user, articleLikeUrlDto.getOriginalUrl(), articleLikeUrlDto.getNews());
         return new ResponseEntity<>(ResponseDto.res(HttpStatus.CREATED, "기사 찜하기 완료"), HttpStatus.CREATED);
     }
 
