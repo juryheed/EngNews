f74651dde9227d6435bc47d78b4f14451557d892
aaahyunseo <hs030608@naver.com>
2024-11-26 22:11:32 +0900

    feat: ArticleLikeUrlDto 저장할 기사의 뉴스 사이트 dto 추가 - #72

--- src/main/java/org/mjulikelion/engnews/dto/request/articleLike/ArticleLikeUrlDto.java
+++ src/main/java/org/mjulikelion/engnews/dto/request/articleLike/ArticleLikeUrlDto.java
@@ -7,4 +7,6 @@ import lombok.Getter;
 public class ArticleLikeUrlDto {
     @NotEmpty(message = "저장할 기사의 url을 입력해주세요.")
     private String originalUrl;
+    @NotEmpty(message = "저장할 기사의 사이트를 입력해주세요.")
+    private String news;
 }
