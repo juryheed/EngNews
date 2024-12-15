52e0cd0444a1d36f77b429810b617f16a8bbf196
juryheed <rabitangl20@naver.com>
2024-12-15 23:10:21 +0900

    feat: analyzeSentence 컨트롤러 추가 - #81

---
+++ src/main/java/org/mjulikelion/engnews/controller/ai/AnalyzeController.java
@@ -0,0 +1,26 @@
+package org.mjulikelion.engnews.controller.ai;
+
+import lombok.RequiredArgsConstructor;
+import org.mjulikelion.engnews.authentication.AuthenticatedUser;
+import org.mjulikelion.engnews.dto.ai.AnalyzeSentenceDto;
+import org.mjulikelion.engnews.dto.ai.FeedbackDto;
+import org.mjulikelion.engnews.dto.response.ResponseDto;
+import org.mjulikelion.engnews.entity.User;
+import org.mjulikelion.engnews.service.AnalyzeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AnalyzeController {
+    private final AnalyzeService analyzeService;
+
+    @PostMapping("/analyze-sentence")
+    public ResponseEntity<ResponseDto<FeedbackDto>> analyzeSentence(@AuthenticatedUser User user, @RequestBody AnalyzeSentenceDto analyzeSentenceDto) {
+        FeedbackDto feedback=analyzeService.analyzeSentence(user,analyzeSentenceDto);
+        return ResponseEntity.ok(ResponseDto.res(HttpStatus.OK, "문장 안에 있는 숙어 및 주요 단어 분석 성공", feedback));
+    }
+}
