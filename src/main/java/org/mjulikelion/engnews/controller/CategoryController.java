e5a822f8b3e958ec1710acfdd7e70fb3a8bc4f8b
aaahyunseo <hs030608@naver.com>
2024-12-16 02:13:36 +0900

    feat: CategoryController 유저 카테고리 별 키워드 옵션 조회 추가 - #87

--- src/main/java/org/mjulikelion/engnews/controller/CategoryController.java
+++ src/main/java/org/mjulikelion/engnews/controller/CategoryController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.mjulikelion.engnews.authentication.AuthenticatedUser;
 import org.mjulikelion.engnews.dto.request.category.CategoryListDto;
 import org.mjulikelion.engnews.dto.response.ResponseDto;
+import org.mjulikelion.engnews.dto.response.category.CategoryInfoDto;
 import org.mjulikelion.engnews.dto.response.category.CategoryListResponseDto;
 import org.mjulikelion.engnews.dto.response.category.UserCategoryListResponseDto;
 import org.mjulikelion.engnews.entity.User;
@@ -13,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -38,6 +40,12 @@ public class CategoryController {
     public ResponseEntity<ResponseDto<Void>> saveCategories(@AuthenticatedUser User user, @RequestBody @Valid CategoryListDto categories) {
         categoryService.saveCategories(user, categories);
         return new ResponseEntity<>(ResponseDto.res(HttpStatus.CREATED, "카테고리 저장 완료"), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/info")
+    public ResponseEntity<ResponseDto<List<CategoryInfoDto>>> getCategoryInfo(@AuthenticatedUser User user){
+        List<CategoryInfoDto> categoryInfoListDto = categoryService.getCategoryInfo(user);
+        return new ResponseEntity<>(ResponseDto.res(HttpStatus.OK, "유저 카테고리 별 키워드 정보 조회 완료", categoryInfoListDto), HttpStatus.OK);
     }
 
     @GetMapping
