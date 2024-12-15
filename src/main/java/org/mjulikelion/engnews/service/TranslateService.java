d9178b3d65062580bda48a4cfed4a2ceb3856765
juryheed <rabitangl20@naver.com>
2024-12-15 22:01:53 +0900

    feat: TranslateService에 translate메서드 추가 - #79

--- src/main/java/org/mjulikelion/engnews/service/TranslateService.java
+++ src/main/java/org/mjulikelion/engnews/service/TranslateService.java
@@ -2,6 +2,7 @@ package org.mjulikelion.engnews.service;
 
 import lombok.RequiredArgsConstructor;
 import org.mjulikelion.engnews.dto.ai.FeedbackDto;
+import org.mjulikelion.engnews.dto.ai.TranslateDto;
 import org.mjulikelion.engnews.dto.ai.TryDto;
 import org.mjulikelion.engnews.entity.User;
 import org.springframework.http.*;
@@ -12,7 +13,8 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class TranslateService {
     private final RestTemplate restTemplate = new RestTemplate();
-    private final String translateURL = "http://43.203.141.103:8000/try-translate";
+    private final String try_TranslateURL = "http://43.203.141.103:8000/try-translate";
+    private final String translateURL = "http://43.203.141.103:8000/translate";
 
     public FeedbackDto tryTranslate(User user, TryDto tryDto){
 
@@ -25,7 +27,7 @@ public class TranslateService {
         HttpEntity<TryDto> entity = new HttpEntity<>(tryDto, headers);
 
         // POST 요청 전송
-        ResponseEntity<String> response = restTemplate.exchange(translateURL, HttpMethod.POST, entity, String.class);
+        ResponseEntity<String> response = restTemplate.exchange(try_TranslateURL, HttpMethod.POST, entity, String.class);
 
 
         FeedbackDto feedback = FeedbackDto.builder()
@@ -34,4 +36,26 @@ public class TranslateService {
 
         return feedback;
     }
+
+    public FeedbackDto translate(User user, TranslateDto translateDto){
+
+        System.out.print(user);
+        // HTTP 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON); //Json형식
+        headers.add("user", String.valueOf(user.getId()));
+
+        // HTTP 요청 엔터티 생성 (헤더 + 바디)
+        HttpEntity<TranslateDto> entity = new HttpEntity<>(translateDto, headers);
+
+        // POST 요청 전송
+        ResponseEntity<String> response = restTemplate.exchange(translateURL, HttpMethod.POST, entity, String.class);
+
+        FeedbackDto feedback = FeedbackDto.builder()
+                .gpt_answer(response.getBody())
+                .build();
+
+        return feedback;
+    }
+
 }
