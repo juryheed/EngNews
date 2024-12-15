7b11deed411fa1a81e006c6006a22119f0e9f841
juryheed <rabitangl20@naver.com>
2024-12-15 22:01:24 +0900

    feat: TranslateController에 translate메서드 추가 - #79

--- src/main/java/org/mjulikelion/engnews/controller/TranslateController.java
+++ src/main/java/org/mjulikelion/engnews/controller/TranslateController.java
@@ -3,6 +3,7 @@ package org.mjulikelion.engnews.controller;
 import lombok.RequiredArgsConstructor;
 import org.mjulikelion.engnews.authentication.AuthenticatedUser;
 import org.mjulikelion.engnews.dto.ai.FeedbackDto;
+import org.mjulikelion.engnews.dto.ai.TranslateDto;
 import org.mjulikelion.engnews.dto.ai.TryDto;
 import org.mjulikelion.engnews.dto.response.ResponseDto;
 import org.mjulikelion.engnews.entity.User;
@@ -11,20 +12,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/try-translate")
 public class TranslateController {
     private final TranslateService translateService;
 
-    @PostMapping
+    @PostMapping("/try-translate")
     public ResponseEntity<ResponseDto<FeedbackDto>> tryTranslate(@AuthenticatedUser User user, @RequestBody TryDto tryDto) {
         FeedbackDto feedback=translateService.tryTranslate(user,tryDto);
         return ResponseEntity.ok(ResponseDto.res(HttpStatus.OK, "기사 번역하고 피드백 받기 성공", feedback));
     }
 
-
+    @PostMapping("/translate")
+    public ResponseEntity<ResponseDto<FeedbackDto>> translate(@AuthenticatedUser User user, @RequestBody TranslateDto translateDto){
+        FeedbackDto feedback=translateService.translate(user,translateDto);
+        return ResponseEntity.ok(ResponseDto.res(HttpStatus.OK, "기사를 한글↔영어 통번역 성공",feedback));
+    }
 }
