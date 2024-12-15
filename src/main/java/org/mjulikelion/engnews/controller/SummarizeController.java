1ed01138f15f734b8ed09f1662b1740d60dd2438
juryheed <rabitangl20@naver.com>
2024-12-15 23:42:30 +0900

    feat: RequestMapping기능 해제 - #82

--- src/main/java/org/mjulikelion/engnews/controller/SummarizeController.java
+++ src/main/java/org/mjulikelion/engnews/controller/SummarizeController.java
@@ -3,7 +3,7 @@ package org.mjulikelion.engnews.controller;
 import lombok.RequiredArgsConstructor;
 import org.mjulikelion.engnews.authentication.AuthenticatedUser;
 import org.mjulikelion.engnews.dto.ai.FeedbackDto;
-import org.mjulikelion.engnews.dto.ai.TryDto;
+import org.mjulikelion.engnews.dto.ai.TrySummarizeDto;
 import org.mjulikelion.engnews.dto.response.ResponseDto;
 import org.mjulikelion.engnews.entity.User;
 import org.mjulikelion.engnews.service.SummarizeService;
@@ -13,14 +13,13 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/try-summarize")
 public class SummarizeController {
 
     private final SummarizeService summarizeService;
 
-    @PostMapping
-    public ResponseEntity<ResponseDto<FeedbackDto>> trySummarize(@AuthenticatedUser User user,@RequestBody TryDto tryDto) {
-        FeedbackDto feedback=summarizeService.trySummarize(user,tryDto);
+    @PostMapping("/try-summarize")
+    public ResponseEntity<ResponseDto<FeedbackDto>> trySummarize(@AuthenticatedUser User user,@RequestBody TrySummarizeDto trySummarizeDto) {
+        FeedbackDto feedback=summarizeService.trySummarize(user,trySummarizeDto);
         return ResponseEntity.ok(ResponseDto.res(HttpStatus.OK, "기사 요약하고 피드백 받기 성공", feedback));
     }
 }
