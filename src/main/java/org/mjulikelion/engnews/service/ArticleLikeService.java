f5cae0f9c886a9d1f58d545b747160e0e583429e
aaahyunseo <hs030608@naver.com>
2024-12-16 18:20:38 +0900

    refactor: ArticleLikeService 필요 없는 주석 삭제 - #94

--- src/main/java/org/mjulikelion/engnews/service/ArticleLikeService.java
+++ src/main/java/org/mjulikelion/engnews/service/ArticleLikeService.java
@@ -13,7 +13,6 @@ import org.mjulikelion.engnews.repository.ArticleLikeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.UUID;
 
 @Service
 @AllArgsConstructor
