6bedfcc19fe22019aaba1d9eb4ec94e114d980e3
juryheed <rabitangl20@naver.com>
2024-12-17 16:26:36 +0900

    feat: 요약 시도하기 로직 변경 - #106

--- src/main/java/org/mjulikelion/engnews/service/ai/SummarizeService.java
+++ src/main/java/org/mjulikelion/engnews/service/ai/SummarizeService.java
@@ -1,8 +1,9 @@
 package org.mjulikelion.engnews.service.ai;
 
 import lombok.RequiredArgsConstructor;
+import org.mjulikelion.engnews.dto.ai.request.TrySummarizeContentDto;
+import org.mjulikelion.engnews.dto.ai.request.TrySummarizeMessageDto;
 import org.mjulikelion.engnews.dto.ai.response.FeedbackDto;
-import org.mjulikelion.engnews.dto.ai.request.TrySummarizeDto;
 import org.mjulikelion.engnews.entity.User;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,7 @@ public class SummarizeService {
     private final RestTemplate restTemplate = new RestTemplate();
     private final String trySummarizeURL = "http://13.209.83.200:8000/try-summarize";
 
-    public FeedbackDto trySummarize(User user, TrySummarizeDto trySummarizeDto){
+    public FeedbackDto trySummarizeContent(User user, TrySummarizeContentDto trySummarizeContentDto){
 
         // HTTP 헤더 설정
         HttpHeaders headers = new HttpHeaders();
@@ -22,7 +23,28 @@ public class SummarizeService {
         headers.add("user", String.valueOf(user.getId()));
 
         // HTTP 요청 엔터티 생성 (헤더 + 바디)
-        HttpEntity<TrySummarizeDto> entity = new HttpEntity<>(trySummarizeDto, headers);
+        HttpEntity<TrySummarizeContentDto> entity = new HttpEntity<>(trySummarizeContentDto, headers);
+
+        // POST 요청 전송
+        ResponseEntity<String> response = restTemplate.exchange(trySummarizeURL, HttpMethod.POST, entity, String.class);
+
+
+        FeedbackDto feedback = FeedbackDto.builder()
+                .gpt_answer(response.getBody())
+                .build();
+
+        return feedback;
+    }
+
+    public FeedbackDto trySummarizeMessage(User user, TrySummarizeMessageDto trySummarizeMessageDto){
+
+        // HTTP 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON); //Json형식
+        headers.add("user", String.valueOf(user.getId()));
+
+        // HTTP 요청 엔터티 생성 (헤더 + 바디)
+        HttpEntity<TrySummarizeMessageDto> entity = new HttpEntity<>(trySummarizeMessageDto, headers);
 
         // POST 요청 전송
         ResponseEntity<String> response = restTemplate.exchange(trySummarizeURL, HttpMethod.POST, entity, String.class);
